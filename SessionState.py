07097eacbdc4d4de57f50c028bd852d4d2a91b9a
hunaidkhan2000 <34857371+hunaidkhan2000@users.noreply.github.com>
2025-07-08 21:24:02 +0530

    Update SessionState.py to new streamlit 1.40.1

--- SessionState.py
+++ SessionState.py
@@ -1,117 +1,56 @@
-"""Hack to add per-session state to Streamlit.
+import streamlit as st
 
-Usage
------
-
->>> import SessionState
->>>
->>> session_state = SessionState.get(user_name='', favorite_color='black')
->>> session_state.user_name
-''
->>> session_state.user_name = 'Mary'
->>> session_state.favorite_color
-'black'
-
-Since you set user_name above, next time your script runs this will be the
-result:
->>> session_state = get(user_name='', favorite_color='black')
->>> session_state.user_name
-'Mary'
-
-"""
-try:
-    import streamlit.ReportThread as ReportThread
-    from streamlit.server.Server import Server
-except Exception:
-    # Streamlit >= 0.65.0
-    import streamlit.report_thread as ReportThread
-    from streamlit.server.server import Server
-
+# Example function that loads some initial data and is cached
+@st.cache_data
+def load_initial_data():
+    # Simulate loading or processing some data
+    return {'user_name': '', 'favorite_color': 'black'}
 
 class SessionState(object):
     def __init__(self, **kwargs):
         """A new SessionState object.
-
         Parameters
         ----------
         **kwargs : any
             Default values for the session state.
-
-        Example
-        -------
-        >>> session_state = SessionState(user_name='', favorite_color='black')
-        >>> session_state.user_name = 'Mary'
-        ''
-        >>> session_state.favorite_color
-        'black'
-
         """
-        for key, val in kwargs.items():
-            setattr(self, key, val)
+        # Load initial data (example function with caching)
+        initial_data = load_initial_data()
+        for key, val in initial_data.items():
+            if key not in st.session_state:
+                st.session_state[key] = val
 
+        for key, val in kwargs.items():
+            if key not in st.session_state:
+                st.session_state[key] = val
+
+    def __setattr__(self, key, value):
+        st.session_state[key] = value
+
+    def __getattr__(self, key):
+        if key in st.session_state:
+            return st.session_state[key]
+        raise AttributeError(f"'{key}' not found in session state")
 
 def get(**kwargs):
     """Gets a SessionState object for the current session.
-
     Creates a new object if necessary.
-
     Parameters
     ----------
     **kwargs : any
         Default values you want to add to the session state, if we're creating a
         new one.
-
-    Example
-    -------
-    >>> session_state = get(user_name='', favorite_color='black')
-    >>> session_state.user_name
-    ''
-    >>> session_state.user_name = 'Mary'
-    >>> session_state.favorite_color
-    'black'
-
-    Since you set user_name above, next time your script runs this will be the
-    result:
-    >>> session_state = get(user_name='', favorite_color='black')
-    >>> session_state.user_name
-    'Mary'
-
     """
-    # Hack to get the session object from Streamlit.
+    for key, default_value in kwargs.items():
+        if key not in st.session_state:
+            st.session_state[key] = default_value
 
-    ctx = ReportThread.get_report_ctx()
-
-    this_session = None
-
-    current_server = Server.get_current()
-    if hasattr(current_server, '_session_infos'):
-        # Streamlit < 0.56
-        session_infos = Server.get_current()._session_infos.values()
-    else:
-        session_infos = Server.get_current()._session_info_by_id.values()
-
-    for session_info in session_infos:
-        s = session_info.session
-        if (
-            # Streamlit < 0.54.0
-            (hasattr(s, '_main_dg') and s._main_dg == ctx.main_dg)
-            or
-            # Streamlit >= 0.54.0
-            (not hasattr(s, '_main_dg') and s.enqueue == ctx.enqueue)
-            or
-            # Streamlit >= 0.65.2
-            (not hasattr(s, '_main_dg') and s._uploaded_file_mgr == ctx.uploaded_file_mgr)
-        ):
-            this_session = s
-
-    if this_session is None:
-        raise RuntimeError(
-            "Oh noes. Couldn't get your Streamlit Session object. "
-            'Are you doing something fancy with threads?')
-
-    # Got the session object! Now let's attach some state into it.
-
-    if not hasattr(this_session, '_custom_session_state'):
-        this_session._custom_session_state = SessionState(**kwargs)
-
-    return this_session._custom_session_state
+    return SessionState()
+'''
+# Example Usage
+session_state = get(user_name='', favorite_color='black')
+if st.button("Set user_name to 'Mary'"):
+    session_state.user_name = 'Mary'
+st.write("User Name:", session_state.user_name)
+st.write("Favorite Color:", session_state.favorite_color)
+'''
